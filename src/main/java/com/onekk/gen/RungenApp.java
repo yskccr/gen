8ffd53b061c020e03c9636650d19a66a6f15c301
WINDOWS-948HJE3\EDZ <yysskk2008@163.com>
2021-02-09 15:39:19 +0800

    <feat> onekk gen

---
+++ src/main/java/com/onekk/gen/RungenApp.java
@@ -0,0 +1,11 @@
+package com.onekk.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RungenApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RungenApp.class,args);
+    }
+}
